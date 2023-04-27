3a7d433e4e842b7a9d93dcf87e9a4899384dfecd
Brunohgs21 <brunohgs21@outlook.com>
2023-04-27 12:40:22 -0300

    feat: create app.ts data-source.ts error.ts and server.ts files

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express, { Application } from "express";
+import { handleErros } from "./error";
+
+const app: Application = express();
+app.use(express.json());
+
+app.use(handleErros);
+
+export default app;
